40a927a8b90a8282f12272185375b34a4590220e
chillyfest <111262007+chillyfest@users.noreply.github.com>
2022-10-01 08:57:40 -0400

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,22 +1,54 @@
 const findSum = function(array) {
-  // your code here - don't forget to return a number!
-};
+    let total = 0;
+    
+    for (let value of array) {
+        total += value;
+    }
+
+return total;
+}
+
+
+
+
 
 const findFrequency = function(array) {
   // your code here - don't forget to return an object!
 };
 
-const isPalindrome = function(str) {
-  // your code here - don't forget to return a boolean!
+
+
+
+
+function isPalindrome(str) {
+
+  var smallStr = str.toLowerCase();
+  
+  var reversed = smallStr.split("").reverse().join("");
+
+  if (reversed === smallStr) return true;
+  return false
 };
+
+
+
+
 
 const largestPair = function(array) {
   // your code here - don't forget to return a number!
 };
 
+
+
+
+
 const removeParenth = function(str) {
   // your code here - don't forget to return a string!
 };
+
+
+
+
 
 const scoreScrabble = function(str) {
   // your code here - don't forget to return a number!
